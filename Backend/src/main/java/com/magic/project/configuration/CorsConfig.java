a3fedbdb7ce0da21515445c037c9a4bc8231833d
adnan-laigroo <131708839+adnan-laigroo@users.noreply.github.com>
2023-06-21 12:40:19 +0530

    Push

--- Backend/src/main/java/com/magic/project/configuration/CorsConfig.java
+++ Backend/src/main/java/com/magic/project/configuration/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")  // Replace with your React app URL
+                .allowedOrigins("http://localhost:3000", "https://hospital-management-connect.netlify.app/")  // Replace with your React app URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
         		.allowCredentials(true);
