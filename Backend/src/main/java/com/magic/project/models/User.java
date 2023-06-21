87aa9b1eae8f747a340846eb15fb3ed0515ef3b2
adnan-laigroo <131708839+adnan-laigroo@users.noreply.github.com>
2023-06-21 13:58:35 +0530

    'Sql

--- Backend/src/main/java/com/magic/project/models/User.java
+++ Backend/src/main/java/com/magic/project/models/User.java
@@ -3,10 +3,12 @@ package com.magic.project.models;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Table(name="User_Details")
 public class User {
 	@Id
 	@NotBlank(message = "Username is required")
