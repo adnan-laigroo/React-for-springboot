1a69298fb7cc3fa39d60db7e1d2cd8ec5626bb9e
adnan-laigroo <131708839+adnan-laigroo@users.noreply.github.com>
2023-06-22 16:31:09 +0530

    Foot

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Homepage from './components/Homepage/Homepage';
+import logo from './Capture.JPG';
+import './footer.css'; // Import the CSS file
 
 const App = () => {
   return (
     <div className="App">
       <Homepage />
+      <footer className="footer">
+        <img src={logo} alt="Logo" className="logo" />
+        <span>React app made by Adnan Laigroo</span>
+      </footer>
     </div>
   );
 };
