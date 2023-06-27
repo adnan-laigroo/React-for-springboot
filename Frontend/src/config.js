0018eaa92bd842a2904837d7d58dc137bdc2108c
Adnan Laigroo <131708839+adnan-laigroo@users.noreply.github.com>
2023-06-27 14:01:28 +0530

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 
-const API_URL = 'http://localhost:8080'; // Replace with the actual backend API URL
-//const API_URL = 'https://hospital-management-connect.onrender.com'; // Replace with the actual backend API URL
+//const API_URL = 'http://localhost:8080'; // Replace with the actual backend API URL
+const API_URL = 'https://hospital-management-connect.onrender.com'; // Replace with the actual backend API URL
 
 export default API_URL;
